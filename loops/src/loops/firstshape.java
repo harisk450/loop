08430af5f36b266a682d38a7c1352ffe70e2f23e
harisk450 <59538783+harisk450@users.noreply.github.com>
2020-01-10 10:10:44 -0500

    shape

---
+++ loops/src/loops/firstshape.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class firstshape {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		
+		for(i=0;i<5;i++) {
+			System.out.println("");
+			for(j=0;j<i;j++) {
+				System.out.print(" @ ");
+			}
+		}
+       
+	}
+
+}
